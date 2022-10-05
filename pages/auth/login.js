95ad70df095e697b86e3d4254cc15f6ab5361a52
Ramazan USLU <ruslu1720@gmail.com>
2022-10-05 18:30:13 +0300

    authentication icin gerekli olan page route yapisi olusturuldu

---
+++ pages/auth/login.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function login() {
+  return (
+    <div>
+      <h1>Login Page Component</h1>
+    </div>
+  );
+}
---
+++ pages/auth/register.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function register() {
+  return (
+    <div>
+      <h1>Register Page Component</h1>
+    </div>
+  );
+}

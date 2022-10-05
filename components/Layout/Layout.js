60b2eab308f009467a71da6737be2e54623ac1ad
Ramazan USLU <ruslu1720@gmail.com>
2022-10-05 16:09:33 +0300

    layout component'i olusturuldu

---
+++ components/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Layout({ children }) {
+  return (
+    <>
+      <div>
+        <main>{children}</main>
+      </div>
+    </>
+  );
+}
+
+export default Layout;
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
 import "../styles/globals.css";
 import "../styles/app.css";
+import Layout from "../components/Layout/Layout";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
 export default MyApp;

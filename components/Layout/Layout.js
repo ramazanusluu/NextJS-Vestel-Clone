a90e201c8f500a87a2f4800f48a531c4a629053e
Ramazan USLU <ruslu1720@gmail.com>
2022-10-05 21:16:28 +0300

    desktopNav icin kategori isimleri menu bar uzerinde listelendi

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../Header/Header";
+import DesktopNav from "../Navbar/DesktopNav/DesktopNav";
 
 function Layout({ children }) {
   return (
     <>
       <Header />
+      <DesktopNav />
       <div>{children}</div>
     </>
   );
--- components/Navbar/DesktopNav/DesktopNav.js
+++ components/Navbar/DesktopNav/DesktopNav.js
@@ -1,10 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function DesktopNav() {
+  const [data, setData] = useState(null);
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch("https://store.vrunibex.com/mobile2/mbProduct/CategoryList")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      });
+  }, []);
+
+  if (isLoading) return <div>Loading</div>;
+  if (!data) return <p>No profile data</p>;
+
+  console.log(data);
   return (
-    <div>
-      <h1>DesktopNav Component</h1>
-    </div>
+    <>
+      <nav className="bg-danger d-none d-xl-block">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <ul className="nav justify-content-center">
+                {data.Result.TreeList.map((item, key) => (
+                  <li key={key} className="nav-item menu-item">
+                    {item.ID < 11 && (
+                      <span className="nav-link menu-link">
+                        {item.DisplayName}
+                      </span>
+                    )}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
   );
 }
 

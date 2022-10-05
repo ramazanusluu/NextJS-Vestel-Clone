525e782b9ffde3b8264af0057b4660d489eb1d63
Ramazan USLU <ruslu1720@gmail.com>
2022-10-05 16:04:14 +0300

    favicon ve page title degistirildi

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
-import '../styles/globals.css'
-import "../styles/App.css"
+import "../styles/globals.css";
+import "../styles/app.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;

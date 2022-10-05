6f5dc67b94e9877a1269d611f27ac8ee651597b5
Ramazan USLU <ruslu1720@gmail.com>
2022-10-05 22:13:58 +0300

    Mobil menu icin gerekli data fetch islemi yapildi

--- components/Navbar/MobileNav/MobileNav.js
+++ components/Navbar/MobileNav/MobileNav.js
@@ -1,6 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Loading from "../../Loading/Loading";
 
 function MobileNav() {
+  const [data, setData] = useState(null);
+  const [isLoading, setLoading] = useState(false);
+  const [selected, setSelected] = useState(null);
+
+  const toggle = (key) => {
+    if (selected === key) {
+      return setSelected(null);
+    }
+    setSelected(key);
+  };
+  useEffect(() => {
+    setLoading(true);
+    fetch("https://store.vrunibex.com/mobile2/mbProduct/CategoryList")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      });
+  }, []);
+
+  if (isLoading) return <Loading />;
+  if (!data) return <p>No profile data</p>;
+
+  console.log(data);
   return (
     <div>
       <h1>MobileNav component</h1>

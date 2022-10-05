88f494b41c402b48f8092ad6352f2da94a2cb580
Ramazan USLU <ruslu1720@gmail.com>
2022-10-05 23:33:53 +0300

    Ana sayfa - Kategorilerin yer aldigi sayfa tamamlandi

--- components/CategoryCard/CategoryCard.js
+++ components/CategoryCard/CategoryCard.js
@@ -1,10 +1,33 @@
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 
-function CategoryCard() {
+function CategoryCard({ item }) {
   return (
-    <div>
-      <h1>CategoryCard component</h1>
-    </div>
+    <>
+      <div className="col-sm-6 col-lg-4 col-xl-3">
+        <Link href={`/category/${item.ID}`}>
+          <a>
+            <div className="card mt-5">
+              <div className="card-head">
+                <Image
+                  src={
+                    item.ImageUri === "" ? "/images/dummy.jpg" : item.ImageUri
+                  }
+                  alt={item.Code}
+                  width={500}
+                  height={500}
+                  title={item.DisplayName}
+                />
+              </div>
+            </div>
+            <div className="card-text card-title my-3 text-center">
+              {item.DisplayName}
+            </div>
+          </a>
+        </Link>
+      </div>
+    </>
   );
 }
 

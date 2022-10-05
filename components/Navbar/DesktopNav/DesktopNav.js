87fb64bd17cbd7f0976833e87d74f11e683c0533
Ramazan USLU <ruslu1720@gmail.com>
2022-10-05 21:32:54 +0300

    dropdown-menu-content alani icin mouseEnter ve mouseLeave event kullanimi

--- components/Navbar/DesktopNav/DesktopNav.js
+++ components/Navbar/DesktopNav/DesktopNav.js
@@ -4,6 +4,19 @@ import Loading from "../../Loading/Loading";
 function DesktopNav() {
   const [data, setData] = useState(null);
   const [isLoading, setLoading] = useState(false);
+  const [selected, setSelected] = useState(null);
+  const toggle = (key) => {
+    if (selected === key) {
+      return setSelected(null);
+    }
+    setSelected(key);
+  };
+  const toggleLeave = (key) => {
+    if (selected === key) {
+      return setSelected(null);
+    }
+    setSelected(null);
+  };
 
   useEffect(() => {
     setLoading(true);
@@ -27,7 +40,11 @@ function DesktopNav() {
             <div className="col-12">
               <ul className="nav justify-content-center">
                 {data.Result.TreeList.map((item, key) => (
-                  <li key={key} className="nav-item menu-item">
+                  <li
+                    key={key}
+                    className="nav-item menu-item"
+                    onMouseEnter={() => toggle(key)}
+                  >
                     {item.ID < 11 && (
                       <span className="nav-link menu-link">
                         {item.DisplayName}
@@ -40,6 +57,32 @@ function DesktopNav() {
           </div>
         </div>
       </nav>
+      {data.Result.TreeList.map((item, key) => (
+        <div
+          key={key}
+          onMouseLeave={() => toggleLeave(key)}
+          className={
+            selected === key ? "dropdown-content show" : "dropdown-content"
+          }
+        >
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-3">
+                {item.SubCategoryList.map((subItem, subKey) => (
+                  <ul key={subKey} className="nav flex-column">
+                    <li className="nav-item">
+                      <span className="nav-link dropdown-list">
+                        <i className="list-item fa-solid fa-chevron-right"></i>{" "}
+                        {subItem.DisplayName}
+                      </span>
+                    </li>
+                  </ul>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
     </>
   );
 }

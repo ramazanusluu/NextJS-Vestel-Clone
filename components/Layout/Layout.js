65a6b161256d3e1b49c22c183fbfe6880ab337a8
Ramazan USLU <ruslu1720@gmail.com>
2022-10-06 09:14:45 +0300

    react-toastify icin gerekli ayarlamalar yapildi & Register form olusturuldu & validasyon islemleri yapildi

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { ToastContainer } from "react-toastify";
 import Header from "../Header/Header";
 import DesktopNav from "../Navbar/DesktopNav/DesktopNav";
 
 function Layout({ children }) {
   return (
     <>
+      <ToastContainer />
       <Header />
       <DesktopNav />
       <div>{children}</div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import "../styles/app.css";
+import "react-toastify/dist/ReactToastify.css";
 import Layout from "../components/Layout/Layout";
 
 function MyApp({ Component, pageProps }) {

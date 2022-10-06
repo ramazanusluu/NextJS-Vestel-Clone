e531ce13d9237a6a9a8a899ddb6d0f098a573c2c
Ramazan USLU <ruslu1720@gmail.com>
2022-10-06 07:49:05 +0300

    ProductCard yapisi olusturuldu ve Product Page sayfasinda listelendi

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import Filter from "./Product/Filter/Filter";
+import List from "./Product/List/List";
 
-function Products() {
+function Products({ data }) {
+  const [list, setList] = useState(data.Result.ProductList);
   return (
-    <div>
-      <h1>Products</h1>
-    </div>
+    <>
+      <div className="container my-5">
+        <h2>{data.Result.CategoryName}</h2>
+        <div className="row">
+          <div className="col-lg-3">
+            <Filter />
+          </div>
+          <div className="col-lg-9">
+            <List list={list} />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 

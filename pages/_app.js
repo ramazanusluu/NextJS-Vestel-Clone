6971ba4ac9096d542848f10af3eb85193c228978
Ramazan USLU <ruslu1720@gmail.com>
2022-10-06 12:31:47 +0300

    AuthContext olusturuldu

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,14 @@ import "../styles/globals.css";
 import "../styles/app.css";
 import "react-toastify/dist/ReactToastify.css";
 import Layout from "../components/Layout/Layout";
+import { AuthProvider } from "../contexts/AuthContext";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
-      <Component {...pageProps} />
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
     </Layout>
   );
 }

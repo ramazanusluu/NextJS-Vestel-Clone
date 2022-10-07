8f2394966bcfee1ec8473c6cdfde376c71f38f4f
Ramazan USLU <ruslu1720@gmail.com>
2022-10-07 10:00:17 +0300

    sepetteki urunlerin mikarı yazildi

--- components/Basket/BasketItem/BasketItem.js
+++ components/Basket/BasketItem/BasketItem.js
@@ -53,7 +53,12 @@ function BasketItem() {
             </div>
           </div>
           <div className="col-lg-2 d-flex align-items-center justify-content-center">
-            Toplam
+            <h6 className="item-price">
+              {item.ActualPriceToShowOnScreen * item.cardQuantity > 1000
+                ? (item.ActualPriceToShowOnScreen * item.cardQuantity) / 1000
+                : item.ActualPriceToShowOnScreen * item.cardQuantity}{" "}
+              TL
+            </h6>
           </div>
           <div className="col-lg-1 text-center text-xl-start">
             <i className="fa-solid fa-xmark"></i>

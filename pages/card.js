de3559ada40b567944bcaa921ab8a12d0309d0eb
Ramazan USLU <ruslu1720@gmail.com>
2022-10-07 14:48:35 +0300

    subTotal component'i hazirlandi

--- pages/card.js
+++ pages/card.js
@@ -1,6 +1,7 @@
 import React from "react";
 import BasketItem from "../components/Basket/BasketItem/BasketItem";
 import EmptyBasket from "../components/Basket/EmptyBasket/EmptyBasket";
+import SubTotal from "../components/Basket/SubTotal/SubTotal";
 import { useBasket } from "../contexts/BasketContext";
 
 export default function card() {
@@ -11,8 +12,11 @@ export default function card() {
       <div className="container my-4">
         <div className="row">
           {items.card.length < 1 && <EmptyBasket />}
-          <div className="col-md-8">
+          <div className="col-lg-8">
             {items.card.length > 0 && <BasketItem />}
+          </div>
+          <div className="col-lg-4">
+            {items.card.length > 0 && <SubTotal />}
           </div>
         </div>
       </div>
